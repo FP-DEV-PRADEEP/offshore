cfd53d253cbe213d0fae4d25d225549cb50f093c
Naveen <naveen@internetbusinesssolutionsindia.com>
2022-01-25 10:57:32 +0530

    title removed

--- src/elements/Hero.js
+++ src/elements/Hero.js
@@ -10,7 +10,7 @@ function Hero() {
             <div className="container">
                 <div className="row align-items-center">
                     <div className="col-md-6 sec-title">
-                        <h2>Welcome Offshore</h2>
+                        {/* <h2>Welcome Offshore</h2> */}
                         <h1>The Leading <span>Offshore IT
                             Staffing</span> Company In India</h1>
                         <p>We provide state-of-the-art outsourcing IT solutions to all scales of businesses at highly competitive prices.</p>
